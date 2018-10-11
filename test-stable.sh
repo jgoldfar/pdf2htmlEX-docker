a5326506cfb4b34c4542c734e2f7cb5b99cd95e6
Jonathan Goldfarb <jgoldfar@gmail.com>
2018-10-11 00:00:32 -0400

    Fixup input & output for scripts [ci skip]

--- test-stable.sh
+++ test-stable.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-if [ $# -eq 0 ]; then
+if [ $# -lt 2 ]; then
   docker run -it jgoldfar/pdf2htmlex-stable:latest /build/usr/bin/pdf2htmlEX --help;
 else
-  docker run -v `pwd`:/data -it jgoldfar/pdf2htmlex-stable:latest /build/usr/bin/pdf2htmlEX /data/$@;
+  docker run -v `pwd`:/data -it jgoldfar/pdf2htmlex-stable:latest /build/usr/bin/pdf2htmlEX --dest-dir /data /data/$1 $2;
 fi
